9b598080af145e64e31ab66f83082edd3a5b82ac
unknown <2327475180@qq.com>
2023-06-12 11:06:39 +0800

    可以选中或取消 tag

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,32 +4,36 @@
       <button>新增标签</button>
     </div>
     <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+      <li
+        v-for="tag in dataSource"
+        :key="tag"
+        :class="{ selected: selectedTags.indexOf(tag) >=0 }"
+        @click="toggle(tag)"
+      >
+        {{ tag }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-export default {};
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class Tags extends Vue {
+  @Prop(Array) dataSource: string[] | undefined;
+  selectedTags: string[] = [];
+
+  toggle(tag: string){
+    const index = this.selectedTags.indexOf(tag)
+    if(index >= 0){
+      this.selectedTags.splice(index, 1)
+    }else {
+      this.selectedTags.push(tag)
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -43,6 +47,8 @@ export default {};
     display: flex;
     flex-wrap: wrap;
     > li {
+      $bg: #d9d9d9;
+      background: $bg;
       background: #d9d9d9;
       height: 24px;
       line-height: 24px;
@@ -50,6 +56,10 @@ export default {};
       padding: 0 16px;
       margin-right: 12px;
       margin-top: 4px;
+      &.selected {
+        background: darken($bg, 50%);
+        color: white;
+      }
     }
   }
   > .new {
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,13 +1,13 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad/>
-    <Types xxx="hi"/>
-    <Notes />
-    <Tags/>
+    <Types/>
+    <Notes/>
+    <Tags :data-source="tags"/>
   </Layout>
 </template>
 
-<script lang="ts">
+<script>
   import NumberPad from '@/components/Money/NumberPad.vue'
   import Types from '@/components/Money/Types.vue';
   import Notes from '@/components/Money/Notes.vue';
@@ -16,6 +16,11 @@
   export default {
     name: 'Money',
     components: {Tags, Notes, Types, NumberPad},
+    data(){
+      return{
+        tags : ['衣','食','住','行']
+      }
+    }
   };
 </script>
 
