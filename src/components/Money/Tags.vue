e16d6e9b605049dd20c2d87037034aa03b873abb
unknown <2327475180@qq.com>
2023-06-12 19:45:10 +0800

    完成Tags组件

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
       <li
@@ -22,7 +22,7 @@ import { Component, Prop } from "vue-property-decorator";
 
 @Component
 export default class Tags extends Vue {
-  @Prop(Array) dataSource: string[] | undefined;
+  @Prop(Array) readonly dataSource: string[] | undefined;
   selectedTags: string[] = [];
 
   toggle(tag: string){
@@ -31,6 +31,21 @@ export default class Tags extends Vue {
       this.selectedTags.splice(index, 1)
     }else {
       this.selectedTags.push(tag)
+    }
+  }
+
+  create(){
+    const name = window.prompt('请输入标签名')
+    if(name === ''){
+      window.alert('不能为空')
+    }else if(this.dataSource){
+      if(this.dataSource.indexOf(name!) >= 0){
+      window.alert('该标签已存在')
+    }else {
+      this.$emit('update:dataSource',
+        [...this.dataSource, name]
+      )
+    }
     }
   }
 }
