7d2e52bde0c3c2c833de6872bbfac5db89655869
unknown <2327475180@qq.com>
2023-06-26 09:52:48 +0800

    添加 EditLabel.vue 的样式，抽离了两个组件 Button 和 FormItem

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,25 +1,36 @@
 <template>
   <Layout>
     <div class="tags">
-      <router-link class="tag" v-for="tag in tags" :key="tag.id" :to="`/labels/edit/${ tag.id }`">
-        <span>{{ tag.name }}</span>
-        <Icon name="right" />
+      <router-link class="tag"
+                   v-for="tag in tags" :key="tag.id"
+                   :to="`/labels/edit/${tag.id}`">
+        <span>{{tag.name}}</span>
+        <Icon name="right"/>
       </router-link>
     </div>
     <div class="createTag-wrapper">
-      <button class="createTag" @click="createTag">新建标签</button>
+      <Button class="createTag"
+              @click="createTag">
+        新建标签
+      </Button>
     </div>
   </Layout>
 </template>
 
 <script lang="ts">
+
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import tagListModel from '@/models/tagListModel';
+  import Button from '@/components/Button.vue';
+
   tagListModel.fetch();
-  @Component
+  @Component({
+    components: {Button}
+  })
   export default class Labels extends Vue {
     tags = tagListModel.data;
+
     createTag() {
       const name = window.prompt('请输出标签名');
       if (name) {
