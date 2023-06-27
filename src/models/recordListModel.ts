def75bc6e21607053a753ebbdcda3985371a83da
unknown <2327475180@qq.com>
2023-06-27 09:49:30 +0800

    将 create 封装到 recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,3 +1,5 @@
+import clone from '@/lib/clone'
+
 const localStorageKeyName = 'recordList';
 const recordListModel = {
   data : [] as RecordItem[],
@@ -7,6 +9,11 @@ const recordListModel = {
   fetch() {
     this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[]
     return this.data;
+  },
+  create(record: RecordItem){
+    const record2: RecordItem = clone(record)
+    record2.createdAt = new Date()
+    this.data.push(record2)
   },
   save() {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
