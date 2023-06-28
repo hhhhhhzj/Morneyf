a25ce8f148d912ef47c932ca3a30ee11267e7cd0
unknown <2327475180@qq.com>
2023-06-28 09:54:24 +0800

    store.count 有 bug 怎么办

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -2,6 +2,10 @@ import recordStore from "./recordStore";
 import tagStore from "./tagStore";
 
 const store = {
+  count: 0,
+  addCount() {
+    this.count += 1;
+  },
   ...recordStore,
   ...tagStore
 };
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -10,6 +10,8 @@
       />
     </div>
     <Tags/>
+    {{count}}
+    <button @click="add">+1</button>
   </Layout>
 </template>
 
@@ -23,10 +25,20 @@ import { Component } from "vue-property-decorator";
 import store from "@/store/index2";
 
 @Component({
-  components: { Tags, FormItem, Types, NumberPad },
+  components: {Tags, FormItem, Types, NumberPad},
+    computed: {
+      count() {
+        return store.count;
+      },
+      recordList() {
+        return store.recordList;
+      }
+    }
 })
 export default class Money extends Vue {
-  recordList = store.recordList;
+  add() {
+      store.addCount();
+    }
   record: RecordItem = {
     tags: [],
     notes: "",
