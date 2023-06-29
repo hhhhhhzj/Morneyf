52ed3e26dcc0c91be319d7b2b0655735969ce958
unknown <2327475180@qq.com>
2023-06-29 16:40:21 +0800

    在 Money.vue 里面使用 Tabs

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -20,9 +20,11 @@ import Types from "@/components/Money/Types.vue";
 import FormItem from "@/components/Money/FormItem.vue";
 import Tags from "@/components/Money/Tags.vue";
 import { Component } from "vue-property-decorator";
+import Tabs from '@/components/Tabs.vue';
+  import recordTypeList from '@/constants/recordTypeList';
 
 @Component({
-  components: { Tags, FormItem, Types, NumberPad },
+  components: {Tabs, Tags, FormItem, Types, NumberPad},
 })
 export default class Money extends Vue {
   record: RecordItem = {
@@ -31,10 +33,12 @@ export default class Money extends Vue {
     type: "-",
     amount: 0,
   };
+  
+  recordTypeList = recordTypeList;
 
   get recordList() {
     return this.$store.state.recordList;
-  }
+  }  
 
   created() {
     this.$store.commit("fetchRecords");
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
-    <Tabs classPrefix="type" 
-          :data-source="typeList" 
+    <Tabs class-prefix="type" 
+          :data-source="recordTypeList" 
           :value.sync="type"/>
     <Tabs class-prefix="interval" 
           :data-source="intervalList" 
@@ -19,6 +19,8 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 import Types from '@/components/Money/Types.vue';
 import Tabs from '@/components/Tabs.vue';
+import intervalList from '@/constants/intervalList';
+  import recordTypeList from '@/constants/recordTypeList';
 
 @Component({
   components: { Types, Tabs }
@@ -26,15 +28,8 @@ import Tabs from '@/components/Tabs.vue';
   export default class Statistics extends Vue {
     type = '-'
     interval = 'day'
-    intervalList = [
-      {text: '按天', value: 'day'},
-      {text: '按周', value: 'week'},
-      {text: '按月', value: 'month'}
-    ]
-    typeList = [
-      {text: '支出', value: '-'},
-      {text: '收入', value: '+'}
-    ]
+    intervalList = intervalList;
+    recordTypeList = recordTypeList
 }
 </script>
 
